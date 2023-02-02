2b29051de1fd93f09ab8d4f2d2af60b9a47f5437
dainiz <dainiz.almazan@gmail.com>
2023-02-02 11:29:52 -0800

    Changes so that methods can be properly accessed

--- back-end/src/main/java/com/dainiz/bestalbumsgenerator/model/lastfm/LastFmData.java
+++ back-end/src/main/java/com/dainiz/bestalbumsgenerator/model/lastfm/LastFmData.java
@@ -1,4 +1,39 @@
 package com.dainiz.bestalbumsgenerator.model.lastfm;
 
+import java.util.Objects;
+
 // This is the outermost layer of the last.fm json response;
-public record LastFmData(Topalbums topalbums) {}
+public final class LastFmData {
+    private final Topalbums topalbums;
+
+    public Topalbums getTopalbums() {
+        return topalbums;
+    }
+
+    public LastFmData(Topalbums topalbums) {
+        this.topalbums = topalbums;
+    }
+
+    public Topalbums topalbums() {
+        return topalbums;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (LastFmData) obj;
+        return Objects.equals(this.topalbums, that.topalbums);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(topalbums);
+    }
+
+    @Override
+    public String toString() {
+        return "LastFmData[" +
+                "topalbums=" + topalbums + ']';
+    }
+}
--- back-end/src/main/java/com/dainiz/bestalbumsgenerator/model/lastfm/Topalbums.java
+++ back-end/src/main/java/com/dainiz/bestalbumsgenerator/model/lastfm/Topalbums.java
@@ -1,7 +1,24 @@
 package com.dainiz.bestalbumsgenerator.model.lastfm;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Topalbums (List<Album> album) {}
+public class Topalbums {
+    @JsonProperty("album")
+    public List<LfAlbum> lfAlbum;
+
+    public Topalbums(List<LfAlbum> lfAlbum) {
+        this.lfAlbum = lfAlbum;
+    }
+
+    public List<LfAlbum> getLfAlbum() {
+        return lfAlbum;
+    }
+
+    public void setLfAlbum(List<LfAlbum> lfAlbum) {
+        this.lfAlbum = lfAlbum;
+    }
+}
