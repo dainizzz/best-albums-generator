1c7dc7020ebaef275eff8f4ac8f8c9ddbd327a98
dainiz <dainiz.almazan@gmail.com>
2023-02-07 16:33:51 -0800

    Removes repetetive code and adds comments for clarity

--- back-end/src/main/java/com/dainiz/bestalbumsgenerator/service/LastFmService.java
+++ back-end/src/main/java/com/dainiz/bestalbumsgenerator/service/LastFmService.java
@@ -16,20 +16,20 @@ public class LastFmService {
     @Autowired
     private RestTemplate lastFmRestTemplate = new RestTemplate();
 
-    public LastFmData getPojoLastFmData(String username) {
-        // TODO: Refactor this to avoid repetition of url
+    private String getLfUrl(String username) {
         Dotenv dotenv = Dotenv.configure().load();
         String params = "&format=json&limit=100&period=12month";
         String apiKey = dotenv.get("LAST_FM_TOKEN");
-        String url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" + username + "&api_key=" + apiKey + params;
+        return "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" + username + "&api_key=" + apiKey + params;
+    }
+
+    public LastFmData getPojoLastFmData(String username) {
+        String url = getLfUrl(username);
         return lastFmRestTemplate.getForObject(url, LastFmData.class);
     }
 
     public Object getRawLastFmData(String username) {
-        Dotenv dotenv = Dotenv.configure().load();
-        String params = "&format=json&limit=100&period=12month";
-        String apiKey = dotenv.get("LAST_FM_TOKEN");
-        String url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" + username + "&api_key=" + apiKey + params;
+        String url = getLfUrl(username);
         return lastFmRestTemplate.getForObject(url, Object.class);
     }
 
@@ -41,50 +41,43 @@ public class LastFmService {
         LastFmData lastFmData = getPojoLastFmData(username);
         Topalbums topalbums = lastFmData.getTopalbums();
         List<LfAlbum> lfAlbumList = topalbums.getLfAlbum();
-        for (LfAlbum lfAlbum: lfAlbumList){
+        for (LfAlbum lfAlbum: lfAlbumList) {
             String mbid = lfAlbum.getMbid();
-            if (mbid == null || mbid.isEmpty() || mbid.isBlank()){
+            if (mbid == null || mbid.isEmpty() || mbid.isBlank()) {
                 continue;
             }
             try {
+                // Can only make 1 request per second to musicbrainz
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             MusicbrainzData mbDataDate = musicbrainzService.getPojoMusicbrainzData(mbid);
             String releaseDate = mbDataDate.getDate();
-            if (releaseDate == null || releaseDate.isEmpty() || releaseDate.isBlank()){
+            if (releaseDate == null || releaseDate.isEmpty() || releaseDate.isBlank()) {
                 continue;
             }
-            String releaseYear = releaseDate.substring(0,4);
+            String releaseYear = releaseDate.substring(0, 4);
             if (releaseYear.equals("2022")) {
                 int playCountInt = Integer.parseInt(lfAlbum.getPlaycount());
                 Artist artist = lfAlbum.getArtist();
                 List<Image> images = lfAlbum.getImage();
                 Image image = images.get(3);
                 Album album = new Album(
-                        mbid, playCountInt, image.getText(), lfAlbum.getName(), artist.name(),releaseDate, 0
+                        mbid, playCountInt, image.getText(), lfAlbum.getName(), artist.name(), releaseDate, 0
                 );
                 topYearlyAlbums.add(album);
             }
+            // Max num of albums = 16
             if (topYearlyAlbums.size() > 15) {
-                 break;
+                break;
             }
+        }
+        // Ensuring album list has an even number of albums
+        if (topYearlyAlbums.size() % 2 != 0){
+            topYearlyAlbums.remove(topYearlyAlbums.size() - 1);
         }
         return topYearlyAlbums;
     }
-
-//     TODO: Finish function - Add filtered albums (topYearlyAlbums) to database
-//    public void addFilteredAlbums(String username, ArrayList<Album> filteredAlbums) {
-//        String sql = "insert into user (first_name, age) values (?, ?)";
-//
-//        List<Object[]> userRows = new ArrayList<Object[]>();
-//        for (User user : userList) {
-//            userRows.add(new Object[] {user.getFirstName(), user.getAge()});
-//        }
-//
-//        jdbcTemplate.batchUpdate(sql, userRows);
-//    }
-
 
 }
