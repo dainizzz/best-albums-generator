b865650f72af7a9e5540dc95ec85bd9f6acc0d10
dainiz <dainiz.almazan@gmail.com>
2023-02-09 23:49:14 -0800

    Removes unnecessary code

--- back-end/src/main/java/com/dainiz/bestalbumsgenerator/repository/AlbumRepository.java
+++ back-end/src/main/java/com/dainiz/bestalbumsgenerator/repository/AlbumRepository.java
@@ -1,12 +1,9 @@
 package com.dainiz.bestalbumsgenerator.repository;
 
 import com.dainiz.bestalbumsgenerator.model.Album;
-import com.dainiz.bestalbumsgenerator.model.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -16,8 +13,5 @@ public interface AlbumRepository extends JpaRepository<Album, Integer> {
     @Transactional
     void deleteByUserId(Integer userId);
 
-//    @Query(
-//            value = "SELECT album FROM Album album WHERE album.user = ?1 ORDER BY album.userRank"
-//    )
     List<Album> findByUserIdOrderByUserRankDesc(Integer userId);
 }
