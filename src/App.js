d355da6982ed9e8ad051c9fb54380fab75346bc4
dainiz <dainiz.almazan@gmail.com>
2023-02-10 18:18:56 -0800

    bug fixes

--- src/App.js
+++ src/App.js
@@ -8,38 +8,50 @@ const App = () => {
   const [usersList, setUsersList] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
   const [albumsList, setAlbumsList] = useState([]);
-  const [currentRound, setCurrentRound] = useState(0);
+  const [currentRound, setCurrentRound] = useState(1);
   const [currentMatches, setCurrentMatches] = useState([]);
 
-  //   const addUserData = (newUser) => {
-  //     axios
-  //       .post(`${process.env.REACT_APP_BACKEND_URL}/users`, newUser)
-  //       .then((response) => {
-  //         console.log(response.data);
-  //         setCurrentUser(response.data);
-  //       })
-  //       .catch((error) => {
-  //         console.log(("Error: ", error));
-  //       });
-  //   };
+  useEffect(() => {
+    console.log("The user has been updated:");
+    console.log({ currentUser });
+    postAlbums(currentUser.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentUser]);
 
-  // V3: async
-  const addUserData = async (newUser) => {
-    await axios
+  const addUserData = (newUser) => {
+    axios
       .post(`${process.env.REACT_APP_BACKEND_URL}/users`, newUser)
       .then((response) => {
-        console.log({ response });
+        console.log(response.data);
         const newUsersList = [...usersList];
         newUsersList.push(response.data);
         setUsersList(newUsersList);
         setCurrentUser(response.data);
-        const userId = response.data.id;
-        postAlbums(userId);
+        console.log({ currentUser });
+        console.log({ usersList });
       })
       .catch((error) => {
         console.log(("Error: ", error));
       });
   };
+
+  // V3: async; now utilizing in tandem w/ useEffect instead lol
+  // const addUserData = async (newUser) => {
+  //   await axios
+  //     .post(`${process.env.REACT_APP_BACKEND_URL}/users`, newUser)
+  //     .then((response) => {
+  //       console.log({ response });
+  //       const newUsersList = [...usersList];
+  //       newUsersList.push(response.data);
+  //       setUsersList(newUsersList);
+  //       setCurrentUser(response.data);
+  //       const userId = response.data.id;
+  //       postAlbums(userId);
+  //     })
+  //     .catch((error) => {
+  //       console.log(("Error: ", error));
+  //     });
+  // };
 
   const postAlbums = async (userId) => {
     await axios
@@ -73,7 +85,7 @@ const App = () => {
   return (
     <>
       {albumsList.length > 0 ? null : <Login addUserData={addUserData} />}
-      {albumsList.length > 0 ? (
+      {albumsList.length > 0 && currentRound !== 5 ? (
         <Game
           albumsList={albumsList}
           setAlbumsList={setAlbumsList}
